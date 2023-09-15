efac8f65affeb5bb7ef0c5e5a8a0fb3e7214ff0e
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-09-14 21:26:13 -0300

    chore: configModule config environment variable validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { PrismaService } from './prisma/prisma.service'
 import { CreateAccountController } from './controllers/create-account.controler'
 import { FetchAccountController } from './controllers/fetch-account.controller'
+import { envSchema } from './env'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
   controllers: [CreateAccountController, FetchAccountController],
   providers: [PrismaService],
 })
