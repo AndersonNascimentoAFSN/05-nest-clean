d62e26879d763302edf729a4b7c64042087e276a
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-09-15 10:06:36 -0300

    feat: add jwt strategy and auth guard of the nestjs/passport

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { FetchAccountController } from './controllers/fetch-account.controller'
 import { envSchema } from './env'
 import { AuthModule } from './auth/auth.module'
 import { AuthenticateController } from './controllers/authenticate.controller'
+import { CreateQuestionController } from './controllers/create-question.controler'
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { AuthenticateController } from './controllers/authenticate.controller'
     CreateAccountController,
     FetchAccountController,
     AuthenticateController,
+    CreateQuestionController,
   ],
   providers: [PrismaService],
 })
