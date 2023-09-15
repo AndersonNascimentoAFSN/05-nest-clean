51b05f8cd28dbce22edc2ea7140d200aec80aed5
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-09-15 09:19:15 -0300

    refactor: change file name

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { CreateAccountController } from './controllers/create-account.controler'
 import { FetchAccountController } from './controllers/fetch-account.controller'
 import { envSchema } from './env'
 import { AuthModule } from './auth/auth.module'
-import { AuthenticateController } from './controllers/authenticate-controller'
+import { AuthenticateController } from './controllers/authenticate.controller'
 
 @Module({
   imports: [
